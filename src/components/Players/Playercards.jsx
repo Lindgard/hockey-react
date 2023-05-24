08b865e237ab6c58cb0fd70b8df2e7a6a5bde9f3
Lindgard <a.b.lindgard@gmail.com>
2023-05-24 13:34:43 +0200

    fixed fetch and render of teams

--- src/components/Players/Playercards.jsx
+++ src/components/Players/Playercards.jsx
@@ -1,31 +1,46 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-function Playercards() {
+
+const Playercards = () => {
   
   // useState som inneholder alle lag via useEffect
-  const [teams, setTeams] = useState([]);
+    const [teams, setTeams] = useState([]);
 
-  let teamsID = [1,2,3,4];
-
-
-  useEffect(() => {
-    for (let i = 0; i < teamsID.length; i++){
-      console.log(teamsID[i])
-    fetch(`https://statsapi.web.nhl.com/api/v1/teams/${teamsID[i]}/roster`)
-      .then(response => response.json())
-      .then(response => {setTeams(response.roster)
-      })
-      .catch(error => setTeams( error ));
-    }}, []);
+    useEffect(() => {
+      const ids = Array.from({length: 55}, (_, i) => i + 1);
+      const requests = ids.map(id => 
+        fetch (`https://statsapi.web.nhl.com/api/v1/teams/${id}/roster`)
+        .then(response => {
+          if (!response.ok) {
+            console.log(`Team ID ${id} not found`);
+          }
+          return response.json();
+        })
+        .then(data => ({id, roster: data.roster}))
+        // .catch(error => console.error('Error:', error))
+        );
+      Promise.all(requests)
+      .then(allTeams => setTeams(allTeams))
+      // .catch(error => console.error('Error:', error))
+      }, []);
+    
     console.log(teams)
     
 
   return (
     <div>
-{/*     {teams.map((player) => (<p key={player.person.id}>{player.person.fullName}</p>))}
- */}     </div>
-    
-  )
+      {teams && teams.map((team, index) => (
+        <div key={index}>
+          <h2>Team ID: {team.id}</h2>
+          {team.roster && team.roster.map((player, index) => (
+            <div key={index}>
+              <p>{player.person.fullName}</p>
+            </div>
+          ))}
+        </div>
+      ))}
+     </div>
+  );
 };
 
 export default Playercards;
--- src/components/Players/SinglePlayerCard.jsx
+++ src/components/Players/SinglePlayerCard.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// hente spiller-ID fra person i arrayet fra Playercards som prop
+// redirecte til SinglePlayerPage
+
 function SinglePlayerCard() {
   return (
     <div>SinglePlayerCard</div>
